bf188d22e99cd7e6ed40ca6469ae6af4bfa559c4
Lan <minenie@gmail.com>
2021-07-10 22:10:57 -0700

    changed to =+

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const buttons = document.querySelectorAll('button')
+
+
+
 const add = function(a,b){
     return a + b;
 }
@@ -17,4 +21,24 @@ const divide = function(a,b){
 const operate = function(operator,num1,num2){
     operator = add(num1,num2) || subtract(num1,num2) || multiply(num1,num2) || divide(num1,num2)
     return operator
+}
+
+const display = function(){
+
+}
+
+
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',returnValue)});
+
+
+
+function returnValue(n){
+    
+    let value = n.target.value;
+    let screen = document.querySelector('.input')
+    screen.textContent=value
+    console.log(value);
+    
 }
