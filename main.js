9704379fc5cdcc14cc66491a07696a30674950d7
Lan <minenie@gmail.com>
2021-07-21 01:01:22 -0700

    + - / *

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 const nums = document.querySelectorAll('.num')
 const operators = document.querySelectorAll('.operator')
 const equalKey = document.querySelector('.is-equals')
+const display = document.querySelector('.input');
+const clearBtn = document.querySelector('.is-clear')
 
 const add = function(a,b){
     return a + b;
@@ -21,17 +23,18 @@ const divide = function(a,b){
 }
 
 const operate = function(operator,num1,num2){
-    if (operator == '+'){
-        return add(num1,num2)
+
+    if (operator === '+'){
+        return add(num1,num2);
     } 
-    if (operator == '-'){
-        return subtract(num1,num2)
+    else if (operator === '-'){
+        return subtract(num1,num2);
     }
-    if (operator == '÷'){
-        return divide(num1,num2)
+    else if (operator === '÷'){
+        return divide(num1,num2);
     }
-    if (operator == '×'){
-        return multiply(num1,num2)
+    else if (operator === '×'){
+        return multiply(num1,num2);
     }
     
 }
@@ -41,8 +44,19 @@ let storeNum = '';
 let clickedOperator = '';
 let secondNum = '';
 let result = '';
-let display = document.querySelector('.input');
+let storeResult ='';
 
+// Clear button
+clearBtn.addEventListener('click', clear);
+function clear(){
+    storeNum = '';
+    clickedOperator = '';
+    secondNum = '';
+    result = '';
+    display.textContent = "0";
+}
+
+// Num Buttons
 nums.forEach((num)=>{
     num.addEventListener('click',returnValue)});
 
@@ -54,22 +68,34 @@ function returnValue(e){
     console.log(storeNum);
 }
 
+// Operator buttons
 operators.forEach((operator =>{
     operator.addEventListener('click',function(){
         secondNum = storeNum;
         clickedOperator = operator.textContent;
         display.textContent = storeNum + clickedOperator;
         storeNum = '';
-        console.log('First' + secondNum + "stored" + storeNum);
+        console.log(`First  ${secondNum}  stored + ${storeNum}`);
         console.log(clickedOperator);
     })
 }));
 
-equalKey.addEventListener('click', function(){
+// calculation function 
+
+const calculate = () => {
     const result = operate((clickedOperator),(secondNum),(storeNum));
     display.textContent = result;
     console.log(result);
-});
+    // storeResult = result;
+    storeNum = result;
+    console.log(`storeNum  ${storeNum}`);
+    console.log("storeResult" +" "+ storeResult);
+}
+
+equalKey.addEventListener('click', calculate);
+
+
+
 
 // function calculate(){
 //     const result = operate((clickedOperator),(secondNum),(storeNum));
