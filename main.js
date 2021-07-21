c6c66a3f440ffb1938e84b10b5db963ab1e6ed88
Lan <minenie@gmail.com>
2021-07-21 01:02:41 -0700

    test check previous

--- main.js
+++ main.js
@@ -1,105 +1,167 @@
 // const { number } = require("yargs");
-
-const nums = document.querySelectorAll('.num')
-const operators = document.querySelectorAll('.operator')
-const equalKey = document.querySelector('.is-equals')
+// Math.round(a / b * 100) / 100
+// @ts-nocheck
+const nums = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const equalKey = document.querySelector('.is-equals');
 const display = document.querySelector('.input');
-const clearBtn = document.querySelector('.is-clear')
+const clearBtn = document.querySelector('.is-clear');
 
-const add = function(a,b){
+
+const add = function (a, b) {
     return a + b;
 }
 
-const subtract = function(a,b){
+const subtract = function (a, b) {
     return a - b;
 }
 
-const multiply = function(a,b){
+const multiply = function (a, b) {
     return a * b;
 }
 
-const divide = function(a,b){
+const divide = function (a, b) {
     return a / b;
 }
 
-const operate = function(operator,num1,num2){
+const operate = function (num1, num2, operator) {
 
-    if (operator === '+'){
-        return add(num1,num2);
-    } 
-    else if (operator === '-'){
-        return subtract(num1,num2);
+    if (operator === '+') {
+        return add(num1, num2);
     }
-    else if (operator === '÷'){
-        return divide(num1,num2);
+    else if (operator === '-') {
+        return subtract(num1, num2);
     }
-    else if (operator === '×'){
-        return multiply(num1,num2);
+    else if (operator === '÷') {
+        return divide(num1, num2);
     }
-    
+    else if (operator === '×') {
+        return multiply(num1, num2);
+    }
+
 }
 
 
+// @ts-nocheck
 let storeNum = '';
 let clickedOperator = '';
 let secondNum = '';
-let result = '';
-let storeResult ='';
+let result = null;
+
 
 // Clear button
 clearBtn.addEventListener('click', clear);
-function clear(){
+function clear() {
     storeNum = '';
-    clickedOperator = '';
+    clickedOperator = undefined;
     secondNum = '';
     result = '';
-    display.textContent = "0";
+    display.textContent = 0;
 }
 
 // Num Buttons
-nums.forEach((num)=>{
-    num.addEventListener('click',returnValue)});
+nums.forEach((num) => {
+    num.addEventListener('click', returnValue)
+});
 
-function returnValue(e){
-    // let button = n.target.id;
-    // screen.textContent=button;
+function returnValue(e) {
     storeNum += e.target.id;
     display.textContent = storeNum;
-    console.log(storeNum);
+    
+
+    console.log(`${typeof (storeNum)} = ${storeNum} << storeNum`);
+ 
 }
 
 // Operator buttons
-operators.forEach((operator =>{
-    operator.addEventListener('click',function(){
-        secondNum = storeNum;
-        clickedOperator = operator.textContent;
+operators.forEach((operator => {
+    operator.addEventListener('click', function (e) {
+
+        clickedOperator = operator.textContent;        
+
         display.textContent = storeNum + clickedOperator;
+        // storeNum = '';
+        
+        if (clickedOperator == '+' && secondNum != '' ){
+            const result = parseFloat(secondNum) + parseFloat(storeNum);
+            display.textContent = result;
+            console.log(result);
+            if (result){
+                storeNum = result;
+            }
+        }
+        else if (clickedOperator == '-' && secondNum != '') {
+            const result = parseFloat(secondNum) - parseFloat(storeNum);
+            display.textContent = result;
+            console.log(result);
+            if (result){
+                storeNum = result;
+            }
+        }
+        else if (clickedOperator == '×' && secondNum != ''){
+            const result = parseFloat(secondNum) * parseFloat(storeNum);
+            display.textContent = result;
+            console.log(result);
+            if (result){
+                storeNum = result;
+            }
+        }
+        else if (clickedOperator == '÷' && secondNum != ''){
+            const result = parseFloat(secondNum) / parseFloat(storeNum);
+            display.textContent = result;
+            console.log(result);
+            if (result){
+                storeNum = result;
+            }
+        }
+        
+        secondNum = storeNum;
+        // storeNum = result;
         storeNum = '';
-        console.log(`First  ${secondNum}  stored + ${storeNum}`);
-        console.log(clickedOperator);
+        
+ 
+       
+        
+    
+        console.log(`SecondNum = ${secondNum}  storedNum = ${storeNum}`);
+        console.log(`${clickedOperator} << click operator`);
+        
+
     })
 }));
 
-// calculation function 
+
+
+// calculation 
 
 const calculate = () => {
-    const result = operate((clickedOperator),(secondNum),(storeNum));
+    
+    const result = operate(Number(secondNum), Number(storeNum), (clickedOperator));
+    secondNum = '';
+
     display.textContent = result;
-    console.log(result);
-    // storeResult = result;
+    console.log(`${typeof (result)} = ${result} << Check type`);
     storeNum = result;
-    console.log(`storeNum  ${storeNum}`);
-    console.log("storeResult" +" "+ storeResult);
+    console.log(`storeNum  ${storeNum} <<< storeNum  result - pressed = >>>>`);
+
 }
 
 equalKey.addEventListener('click', calculate);
 
+function mathOperation(){
+    if (clickedOperator === '×'){
+        result = parseFloat(result) * parseFloat(storeNum);
+        display.textContent = result;
+        
+    }
+};
+
+
+  
 
 
 
-// function calculate(){
-//     const result = operate((clickedOperator),(secondNum),(storeNum));
-//     display.textContent = result;
-//     console.log(result);
-// }
-    
+
+
+
+
